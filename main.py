a675db4d74114bfae36acfc967115bfb894df1ef
Chaostamed <bennbrogan@gmail.com>
2025-10-28 07:57:27 -0400

    Create game window and main loop, handle quitting the game.

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    BLACK = (0, 0, 0)
 
 
+    # Main game loop
+    while True:
+        for event in pygame.event.get(): #Let player quit the game
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(BLACK)  # Fill the screen with black
+        pygame.display.flip() #Update the full display Surface to the screen
 if __name__ == "__main__":
     main()
