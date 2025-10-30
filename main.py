2ce6e3e58612fc7d3c498a89fabe76cfc161df50
Chaostamed <bennbrogan@gmail.com>
2025-10-30 07:58:00 -0400

    Update main.py to work with groups rather than individual objects.

--- main.py
+++ main.py
@@ -15,7 +15,14 @@ def main():
     pygame.init()
     game_clock = pygame.time.Clock() # Create a clock object to manage the frame rate
     dt = 0 #Initialize delta time
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2) #Create player instance at center of screen
+
+    # Create sprite groups for managing game objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # Assign the sprite groups to the Player class and create player
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+
 
     # Set up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -25,9 +32,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt) # Update all updatable objects
+
         screen.fill(BLACK)  # Fill the screen with black
-        player.draw(screen)  # Draw the player
-        player.update(dt)  # Update the player
+
+        for obj in drawable:   # Draw all drawable objects
+            obj.draw(screen)
+
         pygame.display.flip() #Update the full display Surface to the screen
         game_clock.tick(60)  # Cap the frame rate at 60 FPS
         dt = game_clock.get_time() / 1000.0  # Convert milliseconds to seconds
